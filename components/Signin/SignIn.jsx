2f20cd5cc7968fcc5d025575546275d03b4d6b60
ebrahim <myemail@gmail.com>
2023-09-22 11:21:34 +0330

    complete all sign in page with style

--- components/Signin/SignIn.jsx
+++ components/Signin/SignIn.jsx
@@ -1,36 +1,43 @@
 import React from 'react'
-import { AiOutlineEye, AiOutlineLock, AiOutlineMail } from 'react-icons/ai'
+import '/src/styles/style.css'
+import { AiOutlineEye, AiOutlineLock, AiOutlineMail,AiOutlineUsergroupAdd } from 'react-icons/ai'
 export default function Signin() {
+  /////////////onsubmit//////////
+  const handlesubmit =(e)=>{
+    e.preventDefault()
+  }
   return (
     <>
-    <div className="login-container">
-        <div className="login">
+    
+    <div className="signin-container">
+        <div className="signin">
             <div className="logo">
               <img src="/Icon.png" alt="dont" width={169} height={169}/>
             </div>
-           <div className="sigin">
-            <h1>ثبت نام</h1>
-            <form>
+           <div className="right">
+            <h3>ثبت نام</h3>
+            <form onSubmit={handlesubmit}>
                 <div className="email">
                 <input type="text" name='email' />
-                <AiOutlineMail/>
+                <AiOutlineMail size={22}/>
                 </div>
                 <div className="password">
-                    <AiOutlineEye/>
+                    <AiOutlineEye size={22}/>
                     <input type="text" />
-                    <AiOutlineLock/>
-                </div>
-                        <label htmlFor="rule">
-                            <input type="checkbox" />
-                            قوانین سایت را میپذیرم
-                        </label>
+                    <AiOutlineLock size={22}/>
+                </div> 
+                <input type="checkbox" id="rule" name="rule" value="accept rule"/>
+  <label for="rule"> قوانین سایت را میپذیرم</label>
                         <div className="group">
-                            <input type="text" />
+                            <input type="text" name='group code' />
+                            <AiOutlineUsergroupAdd size={22}/>
                         </div>
                         <button className='register-btn'>ثبت نام</button>
-                        <button className='google-login'>ورود باحساب گوگل</button>
-                        <span>
-            حساب کاربری دارید
+                        <button className='google-login'>ورود باحساب گوگل
+                         <img src="/icons/Icon=google.png" alt="not found"  style={{verticalAlign:"middle",marginRight:'0.5rem'}}/>
+                        </button>
+                        <span >
+            حساب کاربری دارید؟
             <a href="#">
                 وارد شوید
             </a>
